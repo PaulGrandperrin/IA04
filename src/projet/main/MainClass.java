e2bbf4574ac79cf2dcd5823f9c3ee93b73673945
karim hamidou <khamidou@gmail.com>
2012-05-17 14:50:49 +0200

    ajouté une fonctioin main

---
+++ src/projet/main/MainClass.java
@@ -0,0 +1,13 @@
+package projet.main;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hop");
+	}
+
+}
