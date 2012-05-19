4e812cc0680f22942fa8dba6c54d29267833b28b
karim hamidou <khamidou@gmail.com>
2012-05-19 19:28:13 +0200

    ajouté le code de libjena + un bout de code pour query une base de connaissance N3.

--- src/agents/MasterAgent.java
+++ src/agents/MasterAgent.java
@@ -1,5 +1,15 @@
 package agents;
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+
 import gui.NetworkGraphFrame;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
@@ -32,6 +42,30 @@ public class MasterAgent extends GuiAgent {
 	}
 
 
+	protected void queryKnowledgeBase(String filename) {
+		OntModel model = ModelFactory.createOntologyModel();
+		model.read("file:" + filename, null, "N3");
+		//model.write(System.out);
+		
+		
+		//Query qu = QueryFactory.read("./requete_base_sur.rq");
+		String req1 = "PREFIX foaf: <http://xmlns.com/foaf/0.1/> select ?user where {?user lgdp:continent .}";
+		Query qu = QueryFactory.create(req1) ;
+		
+		QueryExecution qexec = QueryExecutionFactory.create(qu, model) ;
+		ResultSet results = qexec.execSelect() ;
+		System.out.println(results.hasNext());
+		while(results.hasNext()){
+			QuerySolution sol = results.next() ;
+			//Resource property = sol.getResource("user") ;
+			Resource property = sol.getResource("chose") ;
+			System.out.println(property.getLocalName());
+		}
+
+		qexec.close() ;
+
+	}
+	
 	protected void onGuiEvent(GuiEvent arg0) {
 		// TODO Auto-generated method stub
 		
