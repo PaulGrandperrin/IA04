d57d98e0af4320ba7fb8cc91edd4f3bcf8ed7289
Paul Grandperrin <paul.grandperrin@gmail.com>
2012-05-22 21:36:21 +0200

    Start implementing switchs' link state initialization

--- src/agents/SwitchAgent.java
+++ src/agents/SwitchAgent.java
@@ -1,5 +1,7 @@
 package agents;
 
+import java.util.List;
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.DFService;
@@ -9,6 +11,7 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import messages.AIDSerializer;
 import messages.Message;
+import behaviors.BhvSwitchInfoLink;
 import behaviors.RoutageBhv;
 import behaviors.TopologyBhv;
 
@@ -19,7 +22,9 @@ public class SwitchAgent extends Agent {
 	private static final long serialVersionUID = 7486410164729026372L;
 
 	private GsonBuilder gsonb;
-	
+
+	private List LinkTable;
+
 	protected void setup() {
 //		DFAgentDescription dfd = new DFAgentDescription();
 //		dfd.setName(getAID());
@@ -32,44 +37,39 @@ public class SwitchAgent extends Agent {
 //		} catch (FIPAException e) {
 //			e.printStackTrace();
 //		}
-		
-	
-		gsonb = new GsonBuilder();
-		gsonb.registerTypeAdapter(AID.class, new AIDSerializer());
-		addBehaviour(new RoutageBhv(this,1000));
-		addBehaviour(new TopologyBhv());
-		sendTo(getAID(), "trolol");
+
+		addBehaviour(new BhvSwitchInfoLink(this));
 	}
-	
+
 	public AID searchMasterAgent() {
-		
+
 		AID masterAgent = null;
-		
+
 		DFAgentDescription template = new DFAgentDescription();
 		ServiceDescription sd = new ServiceDescription();
 		sd.setType("MasterAgent");
 		template.addServices(sd);
-		
+
 		try {
 			DFAgentDescription[] result = DFService.search(this, template);
-		
+
 			if (result.length >= 1)
 				masterAgent = result[0].getName();
-			
+
 		} catch (FIPAException fe) {
 			fe.printStackTrace();
 		}
-		
+
 		return masterAgent;
 	}
-	
+
 	public void sendTo(AID dest, String msg) {
 		if (dest == null)
 			return;
-		
-		Gson gson = gsonb.create();		
+
+		Gson gson = gsonb.create();
 		ACLMessage mess = new ACLMessage(ACLMessage.REQUEST);
-		
+
 		AID masterAid = searchMasterAgent();
 		mess.addReceiver(masterAid);
 		Message msgStruct = new Message();
@@ -79,6 +79,14 @@ public class SwitchAgent extends Agent {
 		mess.setContent(gson.toJson(msgStruct));
 		send(mess);
 		System.out.println("message envoyé");
+	}
+
+	public List getLinkTable() {
+		return LinkTable;
+	}
+
+	public void setLinkTable(List linkTable) {
+		LinkTable = linkTable;
 	}
 
 }
--- src/knowledgeBase/QueryKnowledgeBase.java
+++ src/knowledgeBase/QueryKnowledgeBase.java
@@ -24,34 +24,34 @@ public class QueryKnowledgeBase {
 	public QueryKnowledgeBase(String filename) {
 		this.fileName = filename;
 	}
-	
+
 	public Map<String, List<String>> getLinks() {
-		
+
 		Map<String, List<String>> h = new HashMap<String, List<String>>();
-		
+
 		Graph<String, String> g = getGraph();
 		List<String> vertices = new ArrayList<String>(g.getVertices());
-		
+
 		for(int i = 0; i < vertices.size(); i++) {
 			String nom = vertices.get(i);
 			List<String> connections = new ArrayList<String>(g.getSuccessors(nom));
 			h.put(nom, connections);
 		}
-		
+
 		return h;
 	}
-	
+
 	public Graph<String, String> getGraph() {
 		Graph<String, String> graph = new SparseMultigraph<String, String>();
-		
+
 		OntModel model = ModelFactory.createOntologyModel();
 		model.read("file:"+fileName, null, "N3");
-		
+
 		Query qu = QueryFactory.read("requete_base_sur.rq");
-		
+
 		QueryExecution qexec = QueryExecutionFactory.create(qu, model) ;
 		ResultSet results = qexec.execSelect() ;
-		
+
 		int i = 0;
 		while(results.hasNext()){
 			QuerySolution sol = results.next() ;
@@ -60,13 +60,13 @@ public class QueryKnowledgeBase {
 
 			String src = property.getLocalName();
 			String dest = property2.getLocalName();
-			
+
 			if(!graph.containsVertex(src))
 				graph.addVertex(src);
-			
+
 			if(!graph.containsVertex(dest))
 				graph.addVertex(dest);
-			
+
 			graph.addEdge("Edge" + i, src, dest);
 			i++;
 			//System.out.println(property.getLocalName() + " -> " + property2.getLocalName());
@@ -76,8 +76,8 @@ public class QueryKnowledgeBase {
 
 		return graph;
 	}
-	
-	
+
+
 	public static void main(String[] agrs) {
 		QueryKnowledgeBase q = new QueryKnowledgeBase("./network.n3");
 		//q.getGraph();
