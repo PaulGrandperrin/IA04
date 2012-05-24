f3a8e0676e112512f1c5088c3172843957688d51
Deheeger Pierrick <pdeheeger@gmail.com>
2012-05-24 11:50:30 +0200

    ajoute fonctions d'edition de base du graphe

--- src/gui/NetworkGraphFrame.java
+++ src/gui/NetworkGraphFrame.java
@@ -9,7 +9,10 @@ import agents.MasterAgent;
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.EditingModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 
 @SuppressWarnings("serial")
@@ -27,9 +30,18 @@ public class NetworkGraphFrame extends JFrame {
 		Layout<String, String> layout = new CircleLayout<String, String>(g);
 		layout.setSize(new Dimension(300,300)); // sets the initial size of the space
 		// The BasicVisualizationServer<V,E> is parameterized by the edge types
-		BasicVisualizationServer<String,String> vv = new BasicVisualizationServer<String,String>(layout);
+		VisualizationViewer<String,String> vv = new VisualizationViewer<String,String>(layout);
+		
 		vv.setPreferredSize(new Dimension(350,350)); //Sets the viewing area size
 		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+		/*
+		DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
+		gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
+		vv.setGraphMouse(gm);
+		*/
+		EditingModalGraphMouse<String, String> em = new EditingModalGraphMouse<String, String>(
+													    vv.getRenderContext(), null, null, 0, 0);
+		vv.setGraphMouse(em);
 		this.getContentPane().add(vv);
 		this.pack();
 		this.setTitle("Graphe basique");
